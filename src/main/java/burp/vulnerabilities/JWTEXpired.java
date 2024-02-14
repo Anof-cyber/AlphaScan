5854362eca55efe617ec7221ed646955e1e7ccd4
Anof-cyber <kalalsourav18@gmail.com>
2024-02-14 12:28:55 +0530

    jwt debug

--- src/main/java/burp/vulnerabilities/JWTEXpired.java
+++ src/main/java/burp/vulnerabilities/JWTEXpired.java
@@ -17,34 +17,25 @@ public class JWTEXpired {
 
         ArrayList < IScanIssue > issues = new ArrayList < > ();
 
-        byte[] request = base_pair.getRequest();
-        
-        List<String> jwtTokens = extractJWTTokens(request);
-        callbacks.printOutput(String.join(",", jwtTokens));
-       
+        String request = helper.bytesToString(base_pair.getRequest());
 
+        Pattern pattern = Pattern.compile("\\.([\\w-]+)\\.");
+        Matcher matcher = pattern.matcher(request);
 
-        return issues;
-
-
-
-
-    }
-
-
-    public static List<String> extractJWTTokens(byte[] request) {
         List<String> jwtTokens = new ArrayList<>();
-        final String regex = "^((?:\\.?(?:[A-Za-z0-9-_]+)){3})$";
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        final String requestString = new String(request);
 
-        final Matcher matcher = pattern.matcher(requestString);
-
+        // Find JWT tokens in the request
         while (matcher.find()) {
-            jwtTokens.add(matcher.group(0));
+            jwtTokens.add(matcher.group(1));
         }
 
-        return jwtTokens;
+        for (String token : jwtTokens) {
+
+            callbacks.printOutput(token);
+        }
+
+        return issues;
+        
     }
     
 }
